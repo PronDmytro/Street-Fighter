c302c141a73ef71bd8feff6264c4346c45ecfe2b
PronDmytro <dmytropron5838@gmail.com>
2021-05-13 22:21:43 +0300

    .

--- street--fighter/src/constants/controls.js
+++
@@ -1,8 +0,0 @@
-export const controls = {
-  PlayerOneAttack: 'KeyA',
-  PlayerOneBlock: 'KeyD',
-  PlayerTwoAttack: 'KeyJ',
-  PlayerTwoBlock: 'KeyL',
-  PlayerOneCriticalHitCombination: ['KeyQ', 'KeyW', 'KeyE'],
-  PlayerTwoCriticalHitCombination: ['KeyU', 'KeyI', 'KeyO']
-}
--- street--fighter/src/javascript/components/modal/winner.js
+++
@@ -1,3 +0,0 @@
-export function showWinnerModal(fighter) {
-  // call showModal function 
-}
